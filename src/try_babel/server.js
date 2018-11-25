3f68259fd4d45e8b4649073ea725a2857bfb9055
say3no <say3no@gmail.com>
2018-11-25 16:32:50 +0900

    hgoe

--- src/try_babel/server.js
+++ src/try_babel/server.js
@@ -1,1 +1,10 @@
-import express from 'express';
+var express = require('express');
+
+const app = express();
+app.use('/', () => {
+    console.log('aAAAAAAAAaaaaaaaaaaaaaaaaaaaaaa');
+    return 'hello wolrd';
+});
+app.listen(4040, () => {
+    console.log('aaaaaaaakjkdfjkafjkdsjfk');
+});
---
+++ tests/try_babel/server.test.js
@@ -0,0 +1,9 @@
+import {
+    test,
+    expect
+} from 'jest';
+
+
+test('initial test', () => {
+    expect(1).toBe(1);
+});
